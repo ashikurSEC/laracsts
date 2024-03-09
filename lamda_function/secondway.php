befecde75d2f5b6217f7bee6c61050882440e8a1
ashikurSEC <ashikurrahman7194@gmail.com>
2024-03-09 10:22:32 +0600

    lamda function adding

---
+++ lamda_function/secondway.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lambda Function</title>
+</head>
+<body>
+    
+    <?php 
+        $books = [
+            [
+                'name'          => 'Gullivers Travels',
+                'author'        => 'Jonathan Swift',
+                'releaseYear'   => 1726,
+                'parseURL'      => 'http://johanthan.co'
+            ],
+
+            [
+                'name'          => 'Clarissa',
+                'author'        => 'Samuel Richardson',
+                'releaseYear'   => 1748,
+                'parseURL'      => 'http://samuel.co'
+            ],
+
+            [
+                'name'          => 'Tom Jones Tom Jones',
+                'author'        => 'Henry Fielding',
+                'releaseYear'   => 1749,
+                'parseURL'      => 'http://henry.co'
+            ]
+        ];
+
+        function filter($items, $fn){
+            $filterItems = [];
+            foreach ($items as $item) {
+                if ($fn($item)) {
+                    $filterItems[] = $item;
+                }
+            }
+            return $filterItems;
+        }
+
+        $filteredBooks = filter($books, function($book){
+            return $book['releaseYear'] <= 1728;
+        });
+    ?>
+
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['parseURL'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+</html>

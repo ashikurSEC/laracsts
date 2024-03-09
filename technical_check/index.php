746e1088f02485c855d51c01ba8b1bef63ecc5a9
ashikurSEC <ashikurrahman7194@gmail.com>
2024-03-09 18:53:56 +0600

    technical check

---
+++ technical_check/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$busniess = [
+    'name'          => 'Laracasts',
+    'cost'          => 15,
+    'categories'    => ['C', 'PHP', 'JavaScript', 'Python'] 
+];
+
+require_once 'index.view.php';
+
